23be17d8edb7c8703dd1200ce27c91fc6a1805ba
Adam Kraut <adamnkraut@gmail.com>
2012-05-26 01:48:49 -0400

    Added chef plugin; depends on knife

---
+++ starcluster_plugins/chef/chef.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+import os
+
+from starcluster import threadpool
+from starcluster.clustersetup import ClusterSetup
+from starcluster.logger import log
+
+class ClusterChef(ClusterSetup):
+     """Register cluster nodes with a chef server"""
+     def __init__(self, chef_server_url, validation_key, run_list):
+          self.chef_server_url = chef_server_url
+          self.validation_key = open(validation_key, "r").read()
+          self.run_list = run_list
+          log.debug('chef_server_url = %s' % self.chef_server_url)
+          log.debug('run_list = %s' % self.run_list)
+          log.debug('validation_key = %s' % self.validation_key)
+
+    @property
+    def pool(self):
+        if self._pool is None:
+            self._pool = threadpool.get_thread_pool(20, disable_threads=False)
+        return self._pool
+
+     def run(self, nodes, master, user, user_shell, volumes):
+          for node in nodes:
+            log.info('Bootstrapping chef on %s' % node.alias)
+            os.chdir("/Users/kraut/chef-repo")
+            os.system("knife bootstrap %s -N %s -x ubuntu --sudo -r %s" % (node.dns_name, node.alias, self.run_list))
