08b8feedcba4964b8e5537a1fe8e81d4ed691459
Adam Kraut <adamnkraut@gmail.com>
2013-08-22 14:00:48 -0400

    update chef plugin

--- starcluster_plugins/chef/chef.py
+++ starcluster_plugins/chef/chef.py
@@ -1,22 +1,67 @@
-#!/usr/bin/env python
-import os
+import json
 
-from starcluster import threadpool
-from starcluster.clustersetup import ClusterSetup
+from starcluster import clustersetup
 from starcluster.logger import log
 
-class ClusterChef(ClusterSetup):
-     """Register cluster nodes with a chef server"""
-     def __init__(self, chef_server_url, validation_key, run_list):
+class ChefClient(clustersetup.DefaultClusterSetup):
+     """
+     Configures chef-client on StarCluster master node
+
+     Example config:
+     [plugin chef]
+     SETUP_CLASS = chef.ChefClient
+     CHEF_SERVER_URL = https://api.opscode.com/organizations/bioteam
+     VALIDATION_CLIENT_NAME = bioteam-validator
+     VALIDATION_KEY = /Users/kraut/.chef/bioteam-validator.pem
+     RUN_LIST = recipe[ohai]
+
+     """
+     
+     def __init__(self, chef_server_url, validation_client_name, validation_key, run_list):
           self.chef_server_url = chef_server_url
-          self.validation_key = open(validation_key, "r").read()
+          self.validation_client_name = validation_client_name
+          self.validation_key = open(validation_key, 'r').read()
+          self.install_sh = 'https://www.opscode.com/chef/install.sh'
           self.run_list = run_list
           log.debug('chef_server_url = %s' % self.chef_server_url)
+          log.debug('validation_client_name = %s' % self.validation_client_name)
+          # log.debug('validation_key = %s' % self.validation_key)
           log.debug('run_list = %s' % self.run_list)
-          log.debug('validation_key = %s' % self.validation_key)
+
+     def _start_chef(self, node):
+          log.info("Starting chef-client")
+          node.ssh.execute('chef-client -j /etc/chef/first-boot.json')
+
+     def _first_boot_json(self, node):
+          first_boot_json = node.ssh.remote_file('/etc/chef/first-boot.json', 'w')
+          first_boot_json.write(json.dumps({'run_list': self.run_list.split(',')}))
+          first_boot_json.close()
+
+     def _client_config(self, node):
+          log.info("Writing /etc/chef/client.rb")
+          client_rb = node.ssh.remote_file('/etc/chef/client.rb')
+          client_rb.write('\n'.join([
+              "log_level      :auto",
+              "log_location   STDOUT",
+              "chef_server_url '%s'" % self.chef_server_url,
+              "validation_client_name '%s'" % self.validation_client_name,
+              ""
+          ]))
+          client_rb.close()
+
+     def _validation_key(self, node):
+          validation_pem = node.ssh.remote_file('/etc/chef/validation.pem')
+          validation_pem.write(self.validation_key)
+
+     def _install_chef(self, node):
+          log.info("Installing chef")
+          if not node.ssh.isfile('/usr/bin/chef-client'):
+               node.ssh.execute("wget %s && bash install.sh" % self.install_sh)
 
      def run(self, nodes, master, user, user_shell, volumes):
-          for node in nodes:
-            log.info('Bootstrapping chef on %s' % node.alias)
-            os.chdir("/Users/kraut/chef-repo")
-            os.system("knife bootstrap %s -N %s -x ubuntu --sudo -r %s" % (node.dns_name, node.alias, self.run_list))
+          self._install_chef(master)
+          self._validation_key(master)
+          self._client_config(master)
+          self._first_boot_json(master)
+          self._start_chef(master)
+               
